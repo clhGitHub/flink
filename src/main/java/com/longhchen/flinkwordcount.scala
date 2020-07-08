bf510fe07f9ee8f1ff9956ec42b87673c3cbb990
tom <123@123>
2020-07-09 00:16:12 +0800

    第一个程序

---
+++ src/main/java/com/longhchen/flinkwordcount.scala
@@ -0,0 +1,15 @@
+package com.longhchen
+
+/**
+  * Project:flink
+  * Package:com.longhchen
+  * Version 1.0
+  *
+  * Created by chenlonghui on 2020-07-09 0:15
+  */
+object flinkwordcount {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
